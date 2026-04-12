198181bb98050ecbc5ee9b4fa03ac90f93236074
Aman018-gif <kaushalaman1225@gmail.com>
2026-04-12 18:25:32 +0530

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://aman-portfolio-3v4p.vercel.app/',
         changeOrigin: true,
       },
     },
